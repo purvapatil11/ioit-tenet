9fc02586da91ea423c48cc5a8ba2648bc91da916
purvapatil11 <purvapatil.dev@gmail.com>
2026-09-16 17:38:34 +0530

    chore: remove TENET'26 config folder

--- src/config/data/26/gallery.ts
+++
@@ -1,6 +0,0 @@
-
-export const gallery25Images: string[] = Array.from({ length: 94 }, (_, i) =>
-    `https://ioit.acm.org/tenet/25/gallery/img${String(i + 1).padStart(3, "0")}.webp`
-);
-
-export default gallery25Images;
--- src/config/data/26/navbar.ts
+++
@@ -1,9 +0,0 @@
-import type { navbarType } from '@/types';
-import { MUN_PAGE } from '@/config';
-
-export const routes: navbarType[] = [
-  { path: '/techfiesta', name: 'Techfiesta' },
-  { path: MUN_PAGE, name: 'MUN' },
-  { path: 'https://hack.ioittenet.com/', name: 'Hackathon' },
-  { path: '/register/esports', name: 'eSports' },
-];
