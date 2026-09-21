05838f01d314cec9527b831ba47b14891c908cff
Renarin <renarinkholin4@proton.me>
2026-09-21 16:44:44 +0530

    fix: duplicate tenet 25 links (#155)

--- src/modules/home/components/tenetintro/links.tsx
+++ src/modules/home/components/tenetintro/links.tsx
@@ -36,7 +36,7 @@ export const Links: React.FC = () => {
     >
       <div className='flex w-screen flex-row items-center justify-center gap-5 text-xl drop-shadow-md md:w-fit md:flex-col md:text-2xl'>
         <Magnets link='/24/events' className='link-item text-white'>
-          TENET&apos;25
+          TENET&apos;24
         </Magnets>
         <Magnets link='/25/events' className='link-item text-white'>
           TENET&apos;25
